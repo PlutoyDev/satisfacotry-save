d81131c50c2175640a799d3709a209469fbbaf8f
PlutoyDev <plutoy.dev@outlook.com>
2023-12-11 19:14:23 +0800

    add incOffset param to all type reader

--- src/parseSave.ts
+++ src/parseSave.ts
@@ -2,64 +2,94 @@
 Parsing of Satisfactory File
 */
 
-import { readFileSync } from 'fs';
 const UnrealArchiveMagic = 0x9e2a83c1;
 
 class SatisfactoryFileParser {
-  currentOffset: number;
-  dataView: DataView;
+  currentOffset = 0;
+  dataView: DataView | null = null;
 
-  constructor(filename: string) {
-    const buffer = readFileSync(filename).buffer;
-    this.dataView = new DataView(buffer);
-    this.currentOffset = 0;
-  }
+  constructor(filename: string) {}
 
   // Primitives types
-  readChar() {
+  readChar(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getInt8(this.currentOffset);
-    this.currentOffset += 1;
+    if (incOffset) {
+      this.currentOffset += 1;
+    }
     return value;
   }
 
-  readDouble() {
+  readDouble(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getFloat64(this.currentOffset);
-    this.currentOffset += 8;
+    if (incOffset) {
+      this.currentOffset += 8;
+    }
     return value;
   }
 
-  readFloat() {
+  readFloat(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getFloat32(this.currentOffset);
-    this.currentOffset += 4;
+    if (incOffset) {
+      this.currentOffset += 4;
+    }
     return value;
   }
 
-  readUInt32() {
+  readUInt32(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getUint32(this.currentOffset);
-    this.currentOffset += 4;
+    if (incOffset) {
+      this.currentOffset += 4;
+    }
     return value;
   }
 
-  readInt32() {
+  readInt32(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getInt32(this.currentOffset);
-    this.currentOffset += 4;
+    if (incOffset) {
+      this.currentOffset += 4;
+    }
     return value;
   }
 
-  readUInt64() {
+  readUInt64(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getBigUint64(this.currentOffset);
-    this.currentOffset += 8;
+    if (incOffset) {
+      this.currentOffset += 8;
+    }
     return value;
   }
 
-  readInt64() {
+  readInt64(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getBigInt64(this.currentOffset);
-    this.currentOffset += 8;
+    if (incOffset) {
+      this.currentOffset += 8;
+    }
     return value;
   }
 
-  readDuration() {
-    const totalSeconds = this.readInt32();
+  readDuration(incOffset = true) {
+    const totalSeconds = this.readInt32(incOffset);
     return {
       hours: Math.floor(totalSeconds / 3600),
       minutes: Math.floor((totalSeconds % 3600) / 60),
@@ -67,20 +97,17 @@ class SatisfactoryFileParser {
     };
   }
 
-  readFDateTime() {
-    // 100 nanoseconds since 1/1/0001
-    const hundredsNanoseconds = this.readInt64();
-    return new Date(Number(hundredsNanoseconds / 10000n - 11644473600000n));
-  }
-
   // Unreal Types
-  readBool() {
-    const value = this.readInt32();
+  readBool(incOffset = true) {
+    const value = this.readInt32(incOffset);
     return value === 1;
   }
 
-  readFString() {
-    const length = this.readUInt32();
+  readFString(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
+    const length = this.readUInt32(incOffset);
     const value = new TextDecoder().decode(
       this.dataView.buffer.slice(
         this.currentOffset,
@@ -88,37 +115,56 @@ class SatisfactoryFileParser {
       )
     );
     this.currentOffset += length;
+    if (incOffset) {
+    }
+
     return value;
   }
 
-  readFName() {
-    const index = this.readInt32();
-    const number = this.readInt32();
-    const value = this.readFString();
+  readFName(incOffset = true) {
+    const index = this.readInt32(incOffset);
+    const number = this.readInt32(incOffset);
+    const value = this.readFString(incOffset);
     return { index, number, value };
   }
 
-  readFMD5Hash() {
+  readFMD5Hash(incOffset = true) {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     const value = this.dataView.getBigUint64(this.currentOffset);
+    if (incOffset) {
+      this.currentOffset += 8;
+    }
     this.currentOffset += 8;
+
     return value;
   }
 
-  readFText() {
-    const flags = this.readUInt32();
-    const historyType = this.readUInt32();
-    const historyCount = this.readUInt32();
-    const string = this.readFString();
-    const namespace = this.readFName();
+  readFText(incOffset = true) {
+    const flags = this.readUInt32(incOffset);
+    const historyType = this.readUInt32(incOffset);
+    const historyCount = this.readUInt32(incOffset);
+    const string = this.readFString(incOffset);
+    const namespace = this.readFName(incOffset);
     return { flags, historyType, historyCount, string, namespace };
   }
 
-  readFSoftObjectPath() {
-    const value = this.readFString();
+  readFDateTime(incOffset = true) {
+    // 100 nanoseconds since 1/1/0001
+    const hundredsNanoseconds = this.readInt64(incOffset);
+    return new Date(Number(hundredsNanoseconds / 10000n - 11644473600000n));
+  }
+
+  readFSoftObjectPath(incOffset = true) {
+    const value = this.readFString(incOffset);
     return value;
   }
 
   parseSaveHeader() {
+    if (!this.dataView) {
+      throw new Error('No dataView');
+    }
     // Retrieve from C:\Program Files (x86)\Steam\steamapps\common\Satisfactory\CommunityResources\Headers\FGSaveManagerInterface.h#L45
     // int32   SaveHeaderVersion
     // int32   SaveVersion
